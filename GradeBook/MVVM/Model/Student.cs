f6d371db62654ebc9ea86dbaec9eb6991d49d289
dragosc1 <dragos.ciobanu232@gmail.com>
2023-07-27 20:29:31 +0300

    fetched data and implemented some models

---
+++ GradeBook/MVVM/Model/Student.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.MVVM.Model
+{
+    [Table("Student")]
+    public  class Student
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Average { get; set; }
+        public int Absents { get; set; }
+        public int ActivityGrade { get; set; }
+    }
+}
---
+++ GradeBook/MVVM/Model/Student_grade.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.MVVM.Model
+{
+    [Table("Sudent_grade")]
+    public class Student_grade
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Indexed]
+        public int IdStudent { get; set; }
+    }
+}
