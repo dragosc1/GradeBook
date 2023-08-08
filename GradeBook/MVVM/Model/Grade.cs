a5042a57e37effc83768a52aa25bd243eae615ac
dragosc1 <dragos.ciobanu232@gmail.com>
2023-08-08 14:46:13 +0300

    Updated Grade Model

--- GradeBook/MVVM/Model/Grade.cs
+++ GradeBook/MVVM/Model/Grade.cs
@@ -13,10 +13,46 @@ namespace GradeBook.MVVM.Model
     [Table("Grade")]
     public class Grade
     {
+        public Action<string> GradeChanged { get; set; }
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public double Value { get; set; }
-        public DateTime Date { get; set; }
-        public string Information { get; set; }
+
+        private double _value;
+        public double Value
+        {
+            get { return _value; }
+            set { 
+                _value = value;
+                OnGradeChanged(nameof(Value));
+            }
+        }
+
+        private DateTime date;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { 
+                date = value;
+                OnGradeChanged(nameof(Date));
+            }
+        }
+
+        private string information;
+
+        public string Information
+        {
+            get { return information; }
+            set { 
+                information = value;
+                OnGradeChanged(nameof(Information));
+            }
+        }
+
+        private void OnGradeChanged(string v)
+        {
+            GradeChanged?.Invoke(v);
+        }
     }
 }
